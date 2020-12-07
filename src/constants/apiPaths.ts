e59eb36c12a88c764d79de2278d30444ff866403
ALeksei Osin <aleksei_osin@epam.com>
2020-12-07 06:54:19 +0400

    update cart-api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://r9tp5o4mh9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ou3vz8mib5.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://rsaosin-candies-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://c8p4xni64b.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
